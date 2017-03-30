28e440b56f21a36a42c0d19315dd544ce4e49dfd
Harry Saito <hhsaito@gmail.com>
2017-03-30 01:09:53 -0500

    data from db

--- app/components/PropertyMap.jsx
+++ app/components/PropertyMap.jsx
@@ -1,15 +1,17 @@
 // Include React as a dependency
 var React = require("react");
+// Include the Helper (for the saved properties)
+var helpers = require("../utils/helpers");
 
 var INITIAL_LOCATION = {
-  address: 'London, United Kingdom',
+  address: '339 E Chicago Ave, Chicago, IL 60611',
   position: {
-    latitude: 51.5085300,
-    longitude: -0.1257400
+    latitude: 41.8962661,
+    longitude: -87.6186709
   }
 };
 
-var INITIAL_MAP_ZOOM_LEVEL = 8;
+var INITIAL_MAP_ZOOM_LEVEL = 15;
 
 var ATLANTIC_OCEAN = {
   latitude: 29.532804,
@@ -20,62 +22,52 @@ var PropertyMap = React.createClass({
 
   getInitialState: function() {
   	return {
-
-  	  headers: ["Address","Bedrooms","Baths","Asking Price"],
-      data: {"locations":[
-        {"address":"2015 West Eastwood Ave.","bedrooms":"4 bedrooms","baths":"2.5 baths","asking":"$724,900"},
-        {"address":"2235 W Lawrence Ave. #1","bedrooms":"3 bedrooms","baths":"3 baths","asking":"$439,900"},
-        {"address":"2443 W. Winona St.","bedrooms":"5 bedrooms","baths":"2 baths","asking":"$529,900"},
-        {"address":"5057 N Francisco Ave.","bedrooms":"5 bedrooms","baths":"4.5 baths","asking":"$859,000"},
-        {"address":"4247 N Wolcott Ave.","bedrooms":"6 bedrooms","baths":"3 baths","asking":"$695,000"},
-        {"address":"2424 W. Belle Plaine Ave.","bedrooms":"4 bedrooms","baths":"4 baths","asking":"$1,099,000"},
-        {"address":"4220 N Mozart St.","bedrooms":"5 bedrooms","baths":"3.5 baths","asking":"$850,000"}
-      ]},
+      propertydata: [],
       isGeocodingError: false,
       foundAddress: INITIAL_LOCATION.address,
-
   	};
   },
 
   geocodeAddress: function(address) {
+  	let self = this
     this.geocoder.geocode({ 'address': address }, function handleResults(results, status) {
 
       if (status === google.maps.GeocoderStatus.OK) {
 
-        this.setState({
+        self.setState({
           foundAddress: results[0].formatted_address,
           isGeocodingError: false
         });
 
-        this.map.setCenter(results[0].geometry.location);
-        this.marker.setPosition(results[0].geometry.location);
+        self.map.setCenter(results[0].geometry.location);
+        self.marker.setPosition(results[0].geometry.location);
 
         return;
       }
 
-      this.setState({
+      self.setState({
         foundAddress: null,
         isGeocodingError: true
       });
 
-      this.map.setCenter({
+      self.map.setCenter({
         lat: ATLANTIC_OCEAN.latitude,
         lng: ATLANTIC_OCEAN.longitude
       });
 
-      this.marker.setPosition({
+      self.marker.setPosition({
         lat: ATLANTIC_OCEAN.latitude,
         lng: ATLANTIC_OCEAN.longitude
       });
 
-    }.bind(this));
+    });
   },
 
   componentDidMount: function() {
-
-  	var mapElement = this.mapElement;
+    let self = this;
+  	var mapElement = self.mapElement;
     
-    this.map = new google.maps.Map(mapElement, {
+    self.map = new google.maps.Map(mapElement, {
       zoom: INITIAL_MAP_ZOOM_LEVEL,
       center: {
         lat: INITIAL_LOCATION.position.latitude,
@@ -83,21 +75,24 @@ var PropertyMap = React.createClass({
       }
     });
 
-    this.marker = new google.maps.Marker({
-      map: this.map,
+    self.marker = new google.maps.Marker({
+      map: self.map,
       position: {
         lat: INITIAL_LOCATION.position.latitude,
         lng: INITIAL_LOCATION.position.longitude
       }
     });
 
-    this.geocoder = new google.maps.Geocoder();
+    self.geocoder = new google.maps.Geocoder();
+  
+    helpers.getProperties().then(function(propertyData) {
+      self.setState({ propertydata: propertyData.data });
+      console.log("property results", propertyData);
+    });
 
   },
 
-  setMapElementReference: function(mapElementReference) {
-    this.mapElement = mapElementReference;
-  },
+  
 
   render: function () {
     return (
@@ -105,11 +100,12 @@ var PropertyMap = React.createClass({
 	      <h2>Locations</h2>
 	      {this.state.isGeocodingError ? <p className="bg-danger">Address not found.</p> : <p className="bg-info">{this.state.foundAddress}</p>}
 	      <div className="map" ref={this.setMapElementReference}></div>
-	      {this.state.data.locations.map(function(locations, i) {
+	      {this.state.propertydata.map(function(locations, i) {
 	        return (
 	          <p key={i}>{locations.address}, {locations.bedrooms}</p>
 	        );
 	      })}
+	      
       </div>
       // React.DOM.table(null,
       //   React.DOM.thead(null,
